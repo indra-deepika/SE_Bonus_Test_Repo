0a8928dae50b4122b1738bb2fb29fb7c1dbe5c72
Indra Deepika <deepika.iiithyd@gmail.com>
2024-02-23 03:12:44 +0530

    Adding few folders to test automatic refactoring

---
+++ books-web/target/books-web-1.0-SNAPSHOT/app/controller/Login.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * Login controller.
+ */
+App.controller('Login', function($scope, $rootScope, $state, User) {
+  $scope.login = function() {
+    console.log("loggginnnnnn")
+    User.login($scope.user).then(function() {
+      $rootScope.userInfo = User.userInfo(true);
+      $state.transitionTo('book');
+    }, function() {
+      alert('Username or password invalid');
+    });
+  };
+});
